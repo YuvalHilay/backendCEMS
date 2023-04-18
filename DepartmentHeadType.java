f2f4b0dde1bf969792e7c7ee9f12784a912794cd
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2023-04-18 22:48:56 +0300

    Create DepartmentHeadType.java

---
+++ DepartmentHeadType.java
@@ -0,0 +1,7 @@
+package mod;
+
+import java.io.Serializable;
+
+public enum DepartmentHeadType implements Serializable {
+    Math, Physics, ElectricalEngineering, ChemicalEngineering, SoftwareEngineering, CivilEngineering, AerospaceEngineering;
+}
