6fa6c31e0e50eaee9a21b300316bfae863ebbdc2
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2023-04-18 22:36:15 +0300

    Create StudentType.java

---
+++ StudentType.java
@@ -0,0 +1,7 @@
+package mod;
+
+import java.io.Serializable;
+
+public enum StudentType implements Serializable {
+    Math, Physics, ElectricalEngineering, ChemicalEngineering, SoftwareEngineering, CivilEngineering, AerospaceEngineering;
+}
