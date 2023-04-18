9fe4cdb77473c3fce4911c2cfd6cb0e0a9267d2a
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2023-04-18 22:40:25 +0300

    Create LectureType.java

---
+++ LectureType.java
@@ -0,0 +1,8 @@
+
+package mod;
+
+import java.io.Serializable;
+
+public enum LectureType implements Serializable {
+    Math, Physics, ElectricalEngineering, ChemicalEngineering, SoftwareEngineering, CivilEngineering, AerospaceEngineering;
+}
